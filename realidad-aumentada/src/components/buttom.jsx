fdfc8bdad543c1bd75e3f86d139ad106d6e32c0f
Facundo Barros <facubarros6640@gmail.com>
2025-08-29 19:09:49 -0300

    se modifico el componente boton

--- realidad-aumentada/src/components/buttom.jsx
+++ realidad-aumentada/src/components/buttom.jsx
@@ -2,21 +2,3 @@ import React, { Children } from "react";
 import {Link} from 'react-router-dom';
 
 
-const STYLES = ['btn--primary', 'btn--outline'];
-const SIZES = ['btn--medium', 'btn--large'];
-
-export const buttom = ({
-children, type, onClick,buttonStyle, buttomSize
-}) => {
-    const checkbuttonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
-    const checkbuttonSize= SIZES.includes(buttomSize) ? buttomSize : SIZES[0];
-
-    return(
-        <Link to='/camara' classname='btn-mobile'>
-            <buttom classname={'btn ${checkbuttonStyle} ${checkbuttonSize}'} onClick={onclick} type={type}>
-                
-                {children}
-            </buttom>
-        </Link>
-    )
-}
