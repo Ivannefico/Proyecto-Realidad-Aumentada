d172c2e04ab72ac17c918161e0eb2322c8477905
Facundo Barros <facubarros6640@gmail.com>
2025-08-29 19:08:38 -0300

    se modifico el formulario inicio

--- realidad-aumentada/src/components/Formulario_inic.jsx
+++ realidad-aumentada/src/components/Formulario_inic.jsx
@@ -1,128 +1,39 @@
-import React, {useState}from "react"; 
-import {useNavigate} from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../css/Formulario.css";
 
-const Formulario_inic = ({titulo = "Formulario_inic"}) => {
-    const [nombre, setNombre] = useState('');
-    const [apellido, setApellido] = useState('');
-    const [correo, setCorreo] = useState('');
-    const [telefono, setTelefono] = useState('');
-    const [contrasena, setContrasena] = useState('');
-    const [confirmarContrasena, setConfirmarContrasena] = useState('');
-    const navigate = useNavigate();
-
-    const handleSignIn = (e) => {
-        e.preventDefault();
-        navigate("/inicio", {
-            state: { nombre, apellido, correo, telefono, contrasena, confirmarContrasena }
-        });
-    };
-    const handleLogin = (e) => {
-        e.preventDefault();
-        navigate("/resultado", {
-            state: { correo, contrasena }
-        });
-    };
-
-
-    const flip = () => {
-        const front = document.querySelector('.front');
-        const back = document.querySelector('.back');
-        front.style.transform = 'translateX(0%)';
-        back.style.transform = 'translateX(120%)';
-    };
-    const flipBack = () => {
-        const front = document.querySelector('.front');
-        const back = document.querySelector('.back');
-        front.style.transform = 'translateX(-120%)';
-        back.style.transform = 'translateX(0%)';
-    };
-
-    return(
+const Formulario_inic = ({ titulo = "Formulario_inic" }) => {
+    return (
         <div className="form-container">
-              /*Formulario Registro*/ 
-            <form onSubmit={handleSignIn} className="glass-form front">
+            /*Formulario Inicio*/ 
+            <form onSubmit={handleLogin} className="glass-form back">
                 <div className="title-form">
-                    <h2>{titulo}</h2>
-                    <p>¿Ya tienes una cuenta? <button type="button" onClick={flipBack}>Iniciar sesion</button></p>
+                    <h2>Iniciar Sesion</h2>
+                    <p>¿No tienes una cuenta? <button type="button" onClick={flip}>Registrate</button></p>
                 </div>
-                
-                <div className="img"></div>
-
-                <div className="group">
-                    
-                <div className="form-group">
-                    <label>Nombre</label>
-                    <input
-                    required="required"
-                    type="text"
-                    value={nombre}
-                    onChange={(e) => setNombre(e.target.value)}
-                    placeholder="Escribe tu nombre"
-                    />
-                </div>
-
-                <div className="form-group">
-                    <label>Apellido</label>
-                    <input
-                    required="required"
-                    type="text"
-                    value={apellido}
-                    onChange={(e) => setApellido(e.target.value)}
-                    placeholder="Escribe tu apellido"
-                    />
-                </div>
-
                 <div className="form-group">
                     <label>Correo electronico</label>
                     <input
-                    required="required"
-                    type="text"
-                    value={correo}
-                    onChange={(e) => setCorreo(e.target.value)}
-                    placeholder="Escribe tu correo electronico"
+                        required="required"
+                        type="text"
+                        placeholder="Escribe tu correo electronico" 
                     />
                 </div>
-
-                <div className="form-group">
-                    <label>Numero de telefono</label>
-                    <input
-                    required="required"
-                    type="text"
-                    value={telefono}
-                    onChange={(e) => setTelefono(e.target.value)}
-                    placeholder="Escribe tu telefono"
-                    />
-                </div>
-
                 <div className="form-group">
                     <label>Contraseña</label>
                     <input
-                    required="required"
-                    type="password"
-                    value={contrasena}
-                    onChange={(e) => setContrasena(e.target.value)}
-                    placeholder="Escribe tu contraseña"
+                        required="required"
+                        type="password"
+                        placeholder="Escribe tu contraseña"
                     />
                 </div>
-
-                <div className="form-group">
-                    <label>Confirmar Contraseña</label>
-                    <input
-                    required="required"
-                    type="password"
-                     value={confirmarContrasena}
-                    onChange={(e) => setConfirmarContrasena(e.target.value)}
-                    placeholder="Confirma tu contraseña"
-                    />
-                </div>
-                
-
-            <button type="submit">Enviar</button>
-            <button type="button">Google</button>
-
+                <button type="submit">Iniciar Sesion</button>
+            
+                <div className="img"></div>
+            </form>
         </div>
-    </form>
-    </div>
-    )
-}
-export default Formulario_inic
+        
+    );
+};
+
+export default Formulario_inic;
